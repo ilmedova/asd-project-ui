a30a7f3365ca8382af98030fd5717e4b3632c5c3
ilmedova <ilmedovamahri@gmail.com>
2024-12-06 17:15:47 -0600

    added dashboard to header nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 
 const Navbar = ({ isAuthenticated, handleLogout }) => {
     const navigate = useNavigate();
@@ -10,6 +10,9 @@ const Navbar = ({ isAuthenticated, handleLogout }) => {
                 {isAuthenticated ? (
                     <>
                         <li className="space-x-3 font-bold">
+                            <Link className="font-bold" to="/dashboard"></Link>
+                        </li>
+                        <li className="space-x-3 font-bold">
                             <button
                                 onClick={() => {
                                     handleLogout();
@@ -18,19 +21,21 @@ const Navbar = ({ isAuthenticated, handleLogout }) => {
                                 className="hover:text-red-700 flex items-center"
                             >
                                 Logout &nbsp;
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15m3 0 3-3m0 0-3-3m3 3H9" />
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                     strokeWidth={1.5} stroke="currentColor" className="size-6">
+                                    <path strokeLinecap="round" strokeLinejoin="round"
+                                          d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15m3 0 3-3m0 0-3-3m3 3H9"/>
                                 </svg>
                             </button>
                         </li>
                     </>
-                ) : (
+                    ) : (
                     <>
                     </>
-                )}
-            </ul>
-        </nav>
-    );
-};
+                    )}
+                    </ul>
+                    </nav>
+                    );
+                };
 
-export default Navbar;
+                export default Navbar;
